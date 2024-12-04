df450461cacc82d44dfc52d048c566fa32c56d04
barbara99 <barbaraasiamah99@gmail.com>
2024-12-04 12:26:36 +0000

    Added lesson 3

--- basilearn.py
+++ basilearn.py
@@ -16,37 +16,12 @@ class Basilearn:
             choice = input("Enter the number of your choice: ").strip()
 
             if choice == '1':
-                op_lesson()  # Call Lesson 1
+                var_lesson()  # Call Lesson 1
             elif choice == '2':
-                var_lesson()  # Call Lesson 2
+                op_lesson()  # Call Lesson 2
             elif choice == '3':
                 cf_lesson()  # Call Lesson 3
             elif choice == '4':
-                print("Thank you for using the Python Learning Program! Goodbye!")
-                break  # Exit the program
-            else:
-                print("Invalid choice. Please enter 1, 2, or 3.\n")
-
-from lesson_operators import lesson_operators as op_lesson
-from lesson_variables import lesson_variables as var_lesson
-
-class Basilearn:
-    def __call__(self):
-        print("Welcome to the Python Learning Program!\n")
-        
-        while True:
-            print("Choose a lesson to start:")
-            print("1. Lesson 1: Variables and Data Types")
-            print("2. Lesson 2: Operators and Expressions")
-            print("3. Exit the program")
-
-            choice = input("Enter the number of your choice: ").strip()
-
-            if choice == '1':
-                op_lesson()  # Call Lesson 1
-            elif choice == '2':
-                var_lesson()  # Call Lesson 2
-            elif choice == '3':
                 print("Thank you for using the Python Learning Program! Goodbye!")
                 break  # Exit the program
             else:
--- main.py
+++ main.py
@@ -5,11 +5,3 @@ basilearn = Basilearn()
 
 # Call it to run the program
 basilearn()
-
-from basilearn import Basilearn
-
-# Create an instance of the Basilearn class
-basilearn = Basilearn()
-
-# Call it to run the program
-basilearn()
