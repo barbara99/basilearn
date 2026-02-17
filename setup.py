b97786c79e1b4a0a4107f0e7f5d0b7dc40b49ac5
Barbara <barbaraasiamah99@gmail.com>
2026-02-17 16:14:19 +0000

    fix version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with io.open("README.md", encoding='utf-8') as f:
 
 setup(
     name="basilearn",
-    version="0.1.230",
+    version="0.1.2321",
     packages=find_packages(include=["basilearn", "basilearn.*"]),
     install_requires=[
         "setuptools>=56.0.0",
