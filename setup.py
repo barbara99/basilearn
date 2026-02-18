e0acb6a1a20e61dd5b32c9bd321fd2b544facb63
Barbara Asiamah <barbaraasiamah99@gmail.com>
2026-02-18 19:23:00 +0000

    Bump version from 0.1.3 to 0.1.30

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with io.open("README.md", encoding='utf-8') as f:
 
 setup(
     name="basilearn",
-    version="0.1.3",
+    version="0.1.30",
     packages=find_packages(include=["basilearn", "basilearn.*"]),
     install_requires=[
         "setuptools>=56.0.0",
