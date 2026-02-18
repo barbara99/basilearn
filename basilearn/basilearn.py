df1c7d6edfcb42f3e54a94f76530a983628e70c1
Barbara <barbaraasiamah99@gmail.com>
2026-02-18 19:15:43 +0000

    added lesson_functions to basilearn.py, updated setup.py version to 0.1.3

--- basilearn/basilearn.py
+++ basilearn/basilearn.py
@@ -2,6 +2,7 @@ from basilearn.lesson_operators import lesson_operators as op_lesson
 from basilearn.lesson_variables import lesson_variables as var_lesson
 from basilearn.lesson_controlflow import lesson_control_flow as cf_lesson
 from basilearn.lesson_loops import lesson_loops as lps_lesson
+from basilearn.lesson_functions import lesson_functions as ftns_lesson
 
 class Basilearn:
     def __call__(self):
@@ -12,8 +13,9 @@ class Basilearn:
             print("1. Lesson 1: Variables and Data Types")
             print("2. Lesson 2: Operators and Expressions")
             print("3. Lesson 3: Control Flow")
-            print("4. Lesson 4: Loops")            
-            print("5. Exit the program")
+            print("4. Lesson 4: Loops")   
+            print("5. Lesson 5: Functions")         
+            print("6. Exit the program")
 
             choice = input("Enter the number of your choice: ").strip()
 
@@ -24,9 +26,11 @@ class Basilearn:
             elif choice == '3':
                 cf_lesson()  # Call Lesson 3
             elif choice == '4':
-                lps_lesson()  # Call Lesson 4            
+                lps_lesson()  # Call Lesson 4
             elif choice == '5':
+                ftns_lesson()  # Call Lesson 5
+            elif choice == '6':
                 print("Thank you for using the Python Learning Program! Goodbye!")
                 break  # Exit the program
             else:
-                print("Invalid choice. Please enter 1, 2, 3, 4, or 5.\n")
+                print("Invalid choice. Please enter 1, 2, 3, 4, 5, or 6.\n")
